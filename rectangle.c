ef2b4f560ae2986949dde18e59ea0213b5a0116c
catarquaza <iraturner15@gmail.com>
2022-05-03 21:29:51 -0700

    Snvd fsndvjvngjsnbv djs

--- rectangle.c
+++ rectangle.c
@@ -2,24 +2,33 @@
 #include <string.h>
 #include <stdlib.h>
 
+typedef struct 
+{
+    float width;
+    float height;
+} rectangle;
+
+
 int main (void)
 {
+    rectangle rect1;
+
     // prompt user for what to do
     printf ("enter width then hit enter then enter height to find area of a square \n");
 
     // get width from user and convert from a string to an integer
     char inputw [20];
     gets (inputw);
-    int w = atoi(inputw);
+    rect1.width = atof(inputw);
  
     // get height from user and convert from a string to an integer
     char inputh [20];
     gets (inputh);
-    int h = atoi(inputh);
+    rect1.height = atof(inputh);
 
     // calculate the area and print it out
-    int product = h * w;
-    printf ("%d\n",product);
+    float product = rect1.height * rect1.width;
+    printf ("%.2f\n",product);
 }
 
 
