ea61de9aa9ca4e3b1cd3366257f2f46b47c3eec1
catarquaza <iraturner15@gmail.com>
2022-05-08 10:06:16 -0700

    finding space

---
+++ shapes/cube/volume of cube.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct 
+{
+    float width;
+    float height;
+    float length;
+} rectangle;
+
+
+int main (void)
+{
+    rectangle rect1;
+
+    // prompt user for what to do
+    printf ("enter width then hit enter then enter height then enter then enter length to find volume of a cube \n");
+
+    // get width from user and convert from a string to an integer
+    char inputw [20];
+    gets (inputw);
+    rect1.width = atof(inputw);
+ 
+    // get height from user and convert from a string to an integer
+    char inputh [20];
+    gets (inputh);
+    rect1.height = atof(inputh);
+
+    // get lenght from user and convert from a string to an integer
+    char inputl [20];
+    gets (inputl);
+    rect1.length = atof(inputl);
+
+    // calculate the area and print it out
+    float product = rect1.height * rect1.width * rect1.length;
+    printf ("%.2f\n",product);
+}
+
+
---
+++ shapes/rectangle/perimtre of rectangle.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct 
+{
+    float width;
+    float height;
+} rectangle;
+
+
+int main (void)
+{
+    rectangle rect1;
+
+    // prompt user for what to do
+    printf ("enter width then hit enter then enter height to find perimetre of a square \n");
+
+    // get width from user and convert from a string to an integer
+    char inputw [20];
+    gets (inputw);
+    rect1.width = atof(inputw);
+ 
+    // get height from user and convert from a string to an integer
+    char inputh [20];
+    gets (inputh);
+    rect1.height = atof(inputh);
+
+    // calculate the area and print it out
+    float product = rect1.height * 2 + rect1.width * 2;
+    printf ("%.2f\n",product);
+}
+
+
